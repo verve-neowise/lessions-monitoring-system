8f59a45a3fab38960b045796fef383e7291d608d
Neowise <verve.neowise@gmail.com>
2022-10-05 01:39:25 +0500

    permissions

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { Permission, User, PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
@@ -10,25 +10,16 @@ export const findUser = async (username: string) => {
     return prisma.user.findUnique({
         where: {
             username
-        },
-        include: {
-            permissions: true
         }
     })
 }
 
-export const createUser = async (username: string, password: string, permissions: number[]) => {
+export const createUser = async (username: string, password: string, permissions: Permission[]) => {
     return prisma.user.create({
         data: {
             username,
             password,
-            permissions: {
-                connect: permissions.map(id => {
-                    return {
-                        id
-                    }
-                })
-            }
+            permissions
         }
     })
 }
