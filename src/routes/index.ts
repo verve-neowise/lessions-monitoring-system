88650b198d536c33a55bee81062da390d35a3809
verve-neowise <verve.neowise@gmail.com>
2022-10-06 22:30:36 +0500

    snippets

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import errorHandler from '@middlewares/error-handler'
 import authRoutes from './auth.routes'
-import testRoutes from './test.routes'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
-router.use('/test', testRoutes)
 
 router.use(errorHandler)
 
