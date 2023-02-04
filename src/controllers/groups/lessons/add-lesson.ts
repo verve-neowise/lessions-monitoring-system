b0aa34324cd1961c8278b260b614287ed83cd2b1
verve-neowise <verve.neowise@gmail.com>
2023-02-05 00:24:25 +0500

    add group ownship for adding lesson

--- src/controllers/groups/lessons/add-lesson.ts
+++ src/controllers/groups/lessons/add-lesson.ts
@@ -1,10 +1,20 @@
 import { LessonDto, LessonResponse } from '@models/lesson.dto';
 import { Request, Response, NextFunction } from 'express';
 import { createLesson } from '@services/lesson.service'
+import { findGroupById } from '@services/group.service';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const groupId: number = +req.params.id
+        const groupId = +req.params.id
+        const organizationId = +req.params.id
+
+        const group = await findGroupById(organizationId, groupId)
+
+        if (!group) {
+            return res.status(403).json({
+                message: "Group not found: " + groupId
+            })
+        }
 
         const lessonDto: LessonDto = req.body
 
